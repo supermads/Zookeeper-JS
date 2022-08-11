793f8c5818d00d8be83c8d5526b6b5215784a585
supermads <madisonsfahey@gmail.com>
2022-08-11 11:26:08 -0700

    Stage 3

--- Zookeeper/task/main.js
+++ Zookeeper/task/main.js
@@ -1,4 +1,6 @@
-animal = `
+input = require('sync-input');
+
+camel =`
 Switching on the camera in the camel habitat...
  ___.-''''-.
 /___  @    |
@@ -17,10 +19,112 @@ Switching on the camera in the camel habitat...
             ; ;    ! !    ! !     ; ;
            ;,,      !,!   !,!     ;,;
            /_I      L_I   L_I     /_I
-Look at that! Our little camel is sunbathing!`
+Look at that! Our little camel is sunbathing!`;
 
-function animalPrinter(animal){
-    console.log(animal);
-}
+lion = `
+Switching on the camera in the lion habitat...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; \`,       /\`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+,MM:.    .'.-'   .'     ;     \`\\    ;     \`,     MMMMMMMW \`"=./\`-,
+WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / \`=_}
+"^MP__.-'    ,-' _.--""   \`-,   ;       \\  ; ;MMMMMMMMMMW^\`\`; __|
+           /   .'            ; ;         )  )\`{  \\ \`"^W^\`,   \\  :
+          /  .'             /  (       .'  /     Ww._     \`.  \`"
+         /  Y,              \`,  \`-,=,_{   ;      MMMP\`""-,  \`-._.-,
+        (--, )                \`,_ / \`) \\/"")      ^"      \`-, -;"\\:
+The lion is roaring!`;
 
-animalPrinter(animal);
+deer = `
+Switching on the camera in the deer habitat...
+   /|       |\\
+\`__\\\\       //__'
+   ||      ||
+ \\__\`\\     |'__/
+   \`_\\\\   //_'
+   _.,:---;,._
+   \\_:     :_/
+     |@. .@|
+     |     |
+     ,\\.-./ \\
+     ;;\`-'   \`---__________-----.-.
+     ;;;                         \\_\\
+     ';;;                         |
+      ;    |                      ;
+       \\   \\     \\        |      /
+        \\_, \\    /        \\     |\\
+          |';|  |,,,,,,,,/ \\    \\ \\_
+          |  |  |           \\   /   |
+          \\  \\  |           |  / \\  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!`;
+
+goose = `
+Switching on the camera in the goose habitat...
+
+                                    _
+                                ,-"" "".
+                              ,'  ____  \`.
+                            ,'  ,'    \`.  \`._
+   (\`.         _..--.._   ,'  ,'        \\    \\
+  (\`-.\\    .-""        ""'   /          (  d _b
+ (\`._  \`-"" ,._             (            \`-(   \\
+ <_  \`     (  <\`<            \\              \`-._\\
+  <\`-       (__< <           :
+   (__        (_<_<          ;
+    \`------------------------------------------
+The goose is staring intently at you... Maybe it's time to change the channel?`;
+
+bat = `
+Switching on the camera in the bat habitat...
+_________________               _________________
+ ~-.              \\  |\\___/|  /              .-~
+     ~-.           \\ / o o \\ /           .-~
+        >           \\\\  W  //           <
+       /             /~---~\\             \\
+      /_            |       |            _\\
+         ~-.        |       |        .-~
+            ;        \\     /        i
+           /___      /\\   /\\      ___\\
+                ~-. /  \\_/  \\ .-~
+                   V         V
+This bat looks like it’s doing fine.`;
+
+rabbit = `
+Switching on the camera in the rabbit habitat...
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \\
+\\//\\/      .- \\
+ Y        /    Y
+ l       I     !
+ ]\\      _\\    /"\\
+(" ~----( ~   Y.  )
+It looks like we will soon have more rabbits!`;
+
+answer = input("Please enter the number of the habitat you would like to view:");
+
+animalList = [camel, lion, deer, goose, bat, rabbit];
+
+console.log(animalList[answer]);
+console.log(`---
+You've reached the end of the program. To check another habitat, please restart the watcher.`);
--- Zookeeper/task/tests.py
+++ Zookeeper/task/tests.py
@@ -123,14 +123,28 @@ Switching on the camera in the rabbit habitat...
 (" ~----( ~   Y.  )
 It looks like we will soon have more rabbits!"""
 
+animal_index = {
+    '0': (camel, 'camel'),
+    '1': (lion, 'lion'),
+    '2': (deer, 'deer'),
+    '3': (goose, 'goose'),
+    '4': (bat, 'bat'),
+    '5': (rabbit, 'rabbit')
+}
+
+the_end_message = "---\nYou've reached the end of the program. To check another habitat, please restart the watcher."
+
 
 class Zookeeper(StageTest):
     def generate(self):
-        return [TestCase(attach=camel)]
+        tests = [TestCase(stdin=index, attach=(index, animal_index[index])) for index in animal_index]
+        return tests
 
     def check(self, reply, attach):
-        if attach.strip() not in reply.strip():
-            return CheckResult.wrong('You should output a camel!')
+        if attach[1][0].strip() not in reply.strip():
+            return CheckResult.wrong(f'You should output a {attach[1][1]} when the input is the number {attach[0]}')
+        if the_end_message not in reply.strip():
+            return CheckResult.wrong('You should output the message about the end of the program!')
         return CheckResult.correct()
 
 

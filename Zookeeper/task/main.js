898e7a3d2f8181823e85674cf57b17f5c8615428
supermads <madisonsfahey@gmail.com>
2022-08-11 10:37:28 -0700

    Stage 2

--- Zookeeper/task/main.js
+++ Zookeeper/task/main.js
@@ -1,5 +1,26 @@
-console.log("I love animals!");
-console.log("Let's check on the animals...");
-console.log("The deer looks fine.");
-console.log("The bat looks happy.");
-console.log("The lion looks healthy.");
+animal = `
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \\    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!`
+
+function animalPrinter(animal){
+    console.log(animal);
+}
+
+animalPrinter(animal);
